98dd317768321ed12317aac1b5d318c301219071
Wojciech-Kalicinski <wkalicinski94@gmail.com>
2025-02-05 20:01:38 +0100

    Init
    Init

--- Football-Predictions/obj/Debug/net8.0/Football-Predictions.AssemblyInfo.cs
+++ Football-Predictions/obj/Debug/net8.0/Football-Predictions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Football-Predictions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+173c6f6364ae297d63501e631c58a71a2debb991")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96cee01ba10768e7e754efb0b8b7a128f3cc2d98")]
 [assembly: System.Reflection.AssemblyProductAttribute("Football-Predictions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Football-Predictions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
